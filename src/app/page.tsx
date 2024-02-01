8169f9fa3fb2333f2993f3d01cbc25fa23cd0070
apj7486 <apj7486@gmail.com>
2024-02-01 18:06:39 +0100

    routing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 // import AddCompanyButton from '@/app/components/add-company-button';
 import dynamic from 'next/dynamic';
+import MagicButton from '@/app/components/magic-button';
 
 const AddCompanyButton = dynamic(
   () => import('@/app/components/add-company-button'),
@@ -12,6 +13,7 @@ export default function Home() {
     <main>
       <h1 className="text-xl">Home page</h1>
       <AddCompanyButton />
+      <MagicButton />
     </main>
   );
 }
